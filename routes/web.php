5352e48846ba149c6fb9424af3cc05c6f519d30b
haru <haru@example.com>
2024-04-19 12:05:18 +0000

    サンプルページのURLを変更した

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 */
-Route::get('/', [WelcomeController::class, 'index']);
+Route::get('/welcome', [WelcomeController::class, 'index']);
 Route::get('/welcome/second', [WelcomeController::class, 'second']);
 
