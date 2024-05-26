d95b7ca89ab6b30fc7b21d786e837ebc844f4887
haru <haru@example.com>
2024-05-26 01:37:45 +0000

    管理画面の認証と認可を書いた

--- app/Http/Controllers/Admin/AuthController.php
+++ app/Http/Controllers/Admin/AuthController.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\LoginPostRequest;
+use App\Http\Requests\AdminLoginPostRequest;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 
@@ -19,11 +19,12 @@ class AuthController extends Controller
         return view('index');
     }
 
+
     /**
      * ログイン処理
      *
      */
-    public function login(LoginPostRequest $request)
+    public function login(AdminLoginPostRequest $request)
     {
         // validate済
 
@@ -32,18 +33,19 @@ class AuthController extends Controller
         //var_dump($datum); exit;
 
         // 認証
-        if (Auth::attempt($datum) === false) {
+        if (Auth::guard('admin')->attempt($datum) === false) {
             return back()
                    ->withInput() // 入力値の保持
-                   ->withErrors(['auth' => 'emailかパスワードに誤りがあります。',]) // エラーメッセージの出力
+                   ->withErrors(['auth' => 'ログインIDかパスワードに誤りがあります。',]) // エラーメッセージの出力
                    ;
         }
 
-//dd($datum);
         //
         $request->session()->regenerate();
-        return redirect()->route('tasklist');
+        return redirect()->intended('/admin/top');
     }
+
+
 
     /**
      * ログアウト処理
@@ -51,9 +53,9 @@ class AuthController extends Controller
      */
     public function logout(Request $request)
     {
-        Auth::logout();
+        Auth::guard('admin')->logout();
         $request->session()->regenerateToken();  // CSRFトークンの再生成
         $request->session()->regenerate();  // セッションIDの再生成
-        return redirect(route('front.index'));
+        return redirect(route('admin.index'));
     }
 }
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,16 +1,20 @@
-<!DOCTYPE html>
-<html lang="ja">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>ログイン機能付きタスク管理サービス 管理画面</title>
-    </head>
-    <body>
+@extends('admin.layout')
+
+{{-- メインコンテンツ --}}
+@section('contets')
         <h1>管理画面 ログイン</h1>
+        @if ($errors->any())
+            <div>
+            @foreach ($errors->all() as $error)
+                {{ $error }}<br>
+            @endforeach
+            </div>
+        @endif
         <form action="/admin/login" method="post">
-            ログインID：<input name="login_id"><br>
+            @csrf
+            ログインID：<input name="login_id" value="{{ old('login_id') }}"><br>
             パスワード：<input  name="password" type="password"><br>
             <button>ログインする</button>
         </form>
     </body>
-</html>
+@endsection
