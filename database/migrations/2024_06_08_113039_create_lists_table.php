82968b4851e681961ccfd1b58afbeae60d858794
haru <haru@example.com>
2024-06-15 10:13:20 +0000

    0615提出課題_1

--- database/migrations/2024_06_08_113039_create_lists_table.php
+++ database/migrations/2024_06_08_113039_create_lists_table.php
@@ -15,12 +15,9 @@ class CreateTasksTable extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
-            $table->string('name', 128)->comment('タスク名');
-            $table->date('period')->comment('タスクの期限');
-            $table->text('detail')->comment('タスクの詳細');
-            $table->unsignedTinyInteger('priority')->comment('重要度:(1:低い, 2:普通, 3:高い)');
-            $table->unsignedBigInteger('user_id')->comment('このタスクの所有者');
-            $table->foreign('user_id')->references('id')->on('users'); // 外部キー制約
+            $table->string('name', 128)->comment('「買うもの」名');
+            $table->unsignedBigInteger('shopping_list_id')->comment('このタスクの所有者');
+            $table->foreign('hopping_list_id')->references('id')->on('users'); // 外部キー制約
             //$table->timestamps();
             $table->dateTime('created_at')->useCurrent()->comment('タスク完了日時');
             $table->dateTime('updated_at')->useCurrent()->useCurrentOnUpdate();
@@ -35,6 +32,6 @@ class CreateTasksTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('tasks');
+        Schema::dropIfExists('hopping_lists');
     }
 }
